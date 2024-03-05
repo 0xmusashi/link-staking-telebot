b9b099c79f9a41d67448346f141f7f3cb7d01dbc
0xmusashi <musashihp42@gmail.com>
2024-03-05 19:26:10 +0700

    chainlink unstake scan tx bot

---
+++ constants.js
@@ -0,0 +1,12 @@
+const CONTRACT_ADDRESS = '0xBc10f2E862ED4502144c7d632a3459F49DFCDB5e';
+const ADDRESS_EXPLORER_URL = 'https://etherscan.io/address/';
+const TX_EXPLORER_URL = 'https://etherscan.io/tx/'
+
+const RPC = 'https://eth.llamarpc.com';
+
+module.exports = {
+    CONTRACT_ADDRESS,
+    RPC,
+    ADDRESS_EXPLORER_URL,
+    TX_EXPLORER_URL
+}
---
+++ index.js
@@ -0,0 +1,47 @@
+const fs = require('fs/promises');
+const ethers = require("ethers");
+const abi = require("./abi.json");
+require('dotenv').config();
+const TelegramBot = require('node-telegram-bot-api');
+
+const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
+
+const {
+    CONTRACT_ADDRESS,
+    RPC,
+    ADDRESS_EXPLORER_URL,
+    TX_EXPLORER_URL
+} = require('./constants');
+
+const provider = new ethers.providers.JsonRpcProvider(RPC);
+
+const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, provider);
+
+// Function to send an alert message
+async function sendAlert(staker, amount, txHash) {
+    const userUrl = `${ADDRESS_EXPLORER_URL}${staker}`;
+
+    const message =
+        `<b>👨‍🦳 Address <a href="${userUrl}">${staker}</a> unstaked ${amount} $LINK</b>\n\n` +
+        `<b>🔗 TXID 👉 <a href="${TX_EXPLORER_URL}${txHash}">check here</a>\n\n</b>`;
+    const opts = {
+        parse_mode: 'HTML',
+    }
+    await bot.sendMessage(msg.chat.id, message, opts);
+
+}
+
+/*
+Monitor for unstake events
+
+event Unstaked(
+    address indexed staker, uint256 amount, uint256 newStake, uint256 newTotalPrincipal
+);
+*/
+contract.on('Unstaked', async (staker, amount, newStake, newTotalPrincipal, event) => {
+    const logAmount = parseFloat(ethers.utils.formatUnits(amount).toString());
+
+    console.log(`tx: ${event.transactionHash}`);
+
+    await sendAlert(staker, logAmount, event.transactionHash);
+});
